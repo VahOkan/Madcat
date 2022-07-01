e1b43f41da564c0768bcdee1e69c056074e599b6
ShaiNachshon <ShaiNachshon1@gmail.com>
2022-07-01 19:22:29 +0300

    Changed dumb way of damaging player..

--- Assets/Enemy/DmgPlayer.cs
+++ Assets/Enemy/DmgPlayer.cs
@@ -4,13 +4,13 @@ using UnityEngine;
 
 public class DmgPlayer : MonoBehaviour
 {
-    [SerializeField] GameObject parentObject;
+    [SerializeField] EnemyAttackBaseClass parentObject;
     private void OnCollisionEnter(Collision collision)
     {
         print(collision.transform.name);
         if (collision.transform.TryGetComponent(out PlayerHP player))
         {
-            player.TakeDmg(parentObject.GetComponent<EnemyAttackBaseClass>().dmg);
+            player.TakeDmg(parentObject.dmg);
         }
     }
 }
