aebb6417460bbc9dd710b9b9a200f488a62ac097
ShaiNachshon <ShaiNachshon1@gmail.com>
2022-07-13 23:49:49 +0300

    Aim

--- Assets/Enemy/DmgPlayer.cs
+++ Assets/Enemy/DmgPlayer.cs
@@ -5,11 +5,9 @@ using UnityEngine;
 
 public class DmgPlayer : MonoBehaviour
 {
-    
     [SerializeField] private EnemyAttackBaseClass parentObject;
     private void OnCollisionEnter(Collision collision)
     {
-        print(collision.transform.name);
         if (collision.transform.TryGetComponent(out PlayerHP player))
         {
             player.TakeDmg(parentObject.dmg);
--- Assets/Player/PlayerShoot.cs
+++ Assets/Player/PlayerShoot.cs
@@ -5,9 +5,18 @@ using TMPro;
 public class PlayerShoot : MonoBehaviour
 {
     [SerializeField] private Camera mainCamera;
+    [SerializeField] private GameObject normalCameraObject;
+    [SerializeField] private GameObject aimCameraObject;
+    [SerializeField] private GameObject crossHair;
     [SerializeField] TextMeshProUGUI ammoText;
+
+    float timeInCameraTransition = 0;
+    Vector3 lastScaleForZoomIn = new Vector3(5,5,5);
+    Vector3 lastScaleForZoomOut = new Vector3(2.5f,2.5f,2.5f);
+
     private bool isFrozen;
     int ammo;
+    float elapsedTime = 0;
 
     private int _dmg;
     private void Start()
@@ -29,18 +38,59 @@ public class PlayerShoot : MonoBehaviour
         if (isFrozen)
             return;
         
-        if(PlayerShot() && ammo > 0)
+        if(Input.GetKeyDown(KeyCode.Mouse0) && ammo > 0) //if left click is pressed down and we have ammo
+            Shoot();
+
+        if (Input.GetKey(KeyCode.Mouse1))
         {
-            ammo--;
-            ammoText.text = "Ammo: " + ammo;
-            Ray ray = mainCamera.ScreenPointToRay(Input.mousePosition);
-            if (Physics.Raycast(ray, out var hit, Mathf.Infinity))
-                DamageHitEnemy(hit);
+            if (!aimCameraObject.activeInHierarchy) 
+                AimCamera();
+
+            EnlargeCrosshair();
+        }
+        else
+        {
+            if (!normalCameraObject.activeInHierarchy) 
+                NormalCamera();
+
+            ShrinkCrosshair();
         }
     }
-    private bool PlayerShot()
+    void EnlargeCrosshair()
     {
-        return Input.GetKeyDown(KeyCode.Mouse0);
+        elapsedTime += Time.deltaTime;
+        float percentageCompleted = elapsedTime / 0.5f;
+        crossHair.transform.localScale = Vector3.Lerp(lastScaleForZoomOut, new Vector3(5f, 5f, 5f), percentageCompleted);
+        lastScaleForZoomIn = crossHair.transform.localScale;
+        if (percentageCompleted < 1) { timeInCameraTransition += Time.deltaTime; }
+    }
+    void ShrinkCrosshair()
+    {
+        elapsedTime += Time.deltaTime;
+        float percentageCompleted = elapsedTime / timeInCameraTransition;
+        crossHair.transform.localScale = Vector3.Lerp(lastScaleForZoomIn, new Vector3(2.5f, 2.5f, 2.5f), percentageCompleted);
+        lastScaleForZoomOut = crossHair.transform.localScale;
+        if (percentageCompleted >= 1) { timeInCameraTransition = 0; }
+    }
+    void Shoot()
+    {
+        ammo--;
+        ammoText.text = "Ammo: " + ammo;
+        Ray ray = mainCamera.ScreenPointToRay(Input.mousePosition);
+        if (Physics.Raycast(ray, out var hit, Mathf.Infinity))
+            DamageHitEnemy(hit);
+    }
+    void AimCamera()
+    {
+        elapsedTime = 0;
+        normalCameraObject.SetActive(false);
+        aimCameraObject.SetActive(true);
+    }
+    void NormalCamera()
+    {
+        elapsedTime = 0;
+        aimCameraObject.SetActive(false);
+        normalCameraObject.SetActive(true);
     }
     private void DamageHitEnemy(RaycastHit hit)
     {
